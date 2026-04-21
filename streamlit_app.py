6154e4198db0303e03bf2d06b2f21594f1a72c83
artiomcarollo-hub <artiomcarollo@gmail.com>
2026-04-21 23:15:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,2 @@
-import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+from streamlit_mic_recorder import mic_recorder
+audio = mic_recorder(start_prompt="Dettatura", stop_prompt="Stop")
